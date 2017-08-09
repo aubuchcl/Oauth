14a22bcb02b435b80e1b224b1053e34b6fb56d05
Christopher Aubuchon <aubuchcl@gmail.com>
2017-08-09 14:20:53 -0700

    found spelling error only took 5 hours

--- project.py
+++ project.py
@@ -18,6 +18,8 @@ import requests
 
 # declare my client ID by referencing the client secrets file
 CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
+
+APPLICATION_NAME = "Restaurant Menu App"
 #Connect to Database and create database session
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
@@ -34,6 +36,88 @@ def showLogin():
   #session as login_session acts as a dict and we are creating the key value pair here
   login_session['state'] = state
   return render_template('login.html', STATE=state)
+
+  # function that accepts post requests
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+  # do the tokens match?
+  if request.args.get('state') != login_session['state']:
+    response = make_response(json.dumps('Invalid state parameter'),401)
+    response.headers['Content-Type'] = 'application/json'
+    return response
+  # collect the code if the token doesnt fail comparison
+  code = request.data
+  # use the one time code and exchange it for a credentials object which will contain the access token for my server.
+  try:
+    # create the oauth flow object with the secret added.
+    oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+    # one time code flow my server will be sending off
+    oauth_flow.redirect_uri = 'postmessage'
+    # initiate the exchange with my server using the one time code as input
+    credentials = oauth_flow.step2_exchange(code)
+  # If an error occurs along the way then
+  except FlowExchangeError:
+    response = make_response(json.dumps('Failed to upgrade the authorization code.'), 401)
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
+  # check that the access token is valid.
+  access_token = credentials.access_token
+  # append to this url so the google api server can verify this is a valid token for use when sent.
+  url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
+  # here we create a json get request containing the URL and access token.  store the result in a variable called result
+  h = httplib2.Http()
+  result = json.loads(h.request(url, 'GET')[1])
+
+  # if there is an error with result abort
+  if result.get('error') is not None:
+    response = make_response(json.dumps(result.get('error')),500)
+    response.headers['Content-Type'] = 'application/json'
+
+  # Verify that the access token is used for the intended user.
+  gplus_id = credentials.id_token['sub']
+  if result['user_id'] != gplus_id:
+    response = make_response(json.dumps("Token's user ID doesnt match given user ID."), 401)
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
+  # check to see if the user is already logged into the system
+  stored_credentials = login_session.get('credentials')
+  stored_gplus_id = login_session.get('gplus_id')
+  if stored_credentials is not None and gplus_id == stored_gplus_id:
+    response = make_response(json.dumps('Current user is already connected.'),200)
+    response.headers['Content-Type'] = 'application/json'
+
+  # Store the access token in the session for later use.
+  login_session['credentials'] = credentials
+  login_session['gplus_id'] = gplus_id
+
+  #Get user info
+  userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+  params = {'access_token': credentials.access_token, 'alt': 'json'}
+  answer = requests.get(userinfo_url, params=params)
+
+  data = answer.json()
+  # print data
+  # store the user info in my login_session
+  login_session['username'] = data['name']
+  login_session['picture'] = data['picture']
+  login_session['email'] = data['email']
+
+  output = ''
+  output += '<h1> Welcome!</h1>'
+  # output += login_session['picture']
+  # output += ' style = "width:300px; height: 300px; border-radius: 150px; -webkit-border-radius: 150px; -moz-border-radius: 150px;"> '
+  flash('you are now logged in as %s' %login_session['username'])
+  return output
+
+
+
+
+
+
+
+
 
 
 #JSON APIs to view Restaurant Information
