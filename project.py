694d568901a7574f62cddc4daf634d6bc26d4973
Christopher Aubuchon <aubuchcl@gmail.com>
2017-08-09 01:21:53 -0700

    added basic google auth to login.html and the login path now correctly renders the button

--- project.py
+++ project.py
@@ -23,7 +23,7 @@ def showLogin():
   state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
   #session as login_session acts as a dict and we are creating the key value pair here
   login_session['state'] = state
-  return "The current session state is %s" %login_session['state']
+  return render_template('login.html')
 
 
 #JSON APIs to view Restaurant Information
