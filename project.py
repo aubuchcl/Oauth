91b26688663e37af334e2f79b34f56b5c89283e3
Christopher Aubuchon <aubuchcl@gmail.com>
2017-08-10 14:56:36 -0700

    broke something in fbconnect that has to do wiht the token... also the button to log in is not rendering and the redirect is broken

--- project.py
+++ project.py
@@ -28,9 +28,10 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
+
 #Create a state token to prevent request forgery.
 #Store it in the session for later validation.
-@app.route('/login/')
+@app.route('/login')
 def showLogin():
   state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
   #session as login_session acts as a dict and we are creating the key value pair here
@@ -89,8 +90,10 @@ def gconnect():
     response.headers['Content-Type'] = 'application/json'
 
   # Store the access token in the session for later use.
+  login_session['provider'] = 'google'
   login_session['credentials'] = credentials
   login_session['gplus_id'] = gplus_id
+  response = make_response(json.dumps('Successfully connected user.', 200))
 
   #Get user info
   userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
@@ -110,9 +113,13 @@ def gconnect():
   login_session['user_id'] = user_id
 
   output = ''
-  output += '<h1> Welcome!</h1>'
-  # output += login_session['picture']
-  # output += ' style = "width:300px; height: 300px; border-radius: 150px; -webkit-border-radius: 150px; -moz-border-radius: 150px;"> '
+  output += '<h1>Welcome, '
+  output += login_session['username']
+  output += '!</h1>'
+  output += '<img src="'
+  output += login_session['picture']
+  output += ' " style = "width: 300px; height: 300px; border-radius: 150px;-webkit-border-radius: 150px; -moz-border-radius: 150px;">'
+
   flash('you are now logged in as %s' %login_session['username'])
   return output
 
@@ -146,6 +153,101 @@ def gdisconnect():
     response = make_response(json.dumps('Failed to revoke token for given user.'), 400)
     response.headers['Content-Type'] = 'application/json'
     return response
+
+
+
+@app.route('/fbconnect', methods=['POST'])
+def fbconnect():
+  if request.args.get('state') != login_session['state']:
+    response = make_response(json.dumps('Invalid state parameter.'), 401)
+    response.headers['Content-Type'] = 'application/json'
+    return response
+  access_token = request.data
+
+  # get a long live token from short live token
+  app_id = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_id']
+  app_secret = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+  url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (app_id, app_secret, access_token)
+  h = httplib2.Http()
+  result = h.request(url, 'GET')[1]
+
+  # use token to get user info from the API
+  userinfo_url = 'https://graph.facebook.com/v2.2/me'
+  # strip expire tag from access token
+  token = result.split('&')[0]
+
+  url = 'https://graph.facebook.com/v2.8/me?%s&fields=name,id,email' % token
+  h = httplib2.Http()
+  result = h.request(url, 'GET')[1]
+
+  data = json.loads(result)
+
+  login_session['provider'] = 'facebook'
+  login_session['username'] = data['name']
+  login_session['email'] = data['email']
+  login_session['facebook_id'] = data['id']
+
+
+  # facebook uses separate api call for pic
+  url = 'https://graph.facebook.com/v2.2/me/picture?%s&redirect=0&height=200&width=200' % token
+  h = httplib2.Http()
+  result = h.request(url, 'GET')[1]
+  data = json.loads(result)
+  login_session['picture'] = data['data']['url']
+
+  # see if user exists
+  user_id = getUserID(login_session['email'])
+  if not user_id:
+    user_id = CreateUser(login_session)
+  login_Session['user_id'] = user_id
+
+  output = ''
+  output += '<h1>Welcome, '
+  output += login_session['username']
+  output += '!</h1>'
+  output += '<img src="'
+  output += login_session['picture']
+  output += ' " style = "width: 300px; height: 300px; border-radius: 150px;-webkit-border-radius: 150px; -moz-border-radius: 150px;">'
+
+  flash('Now logged in as %s' % login_session['username'])
+  return output
+
+@app.route('/fbdisconnect')
+def fbdisconnect():
+  facebook_id = login_session['facebook_id']
+  url = 'https://graph.facebook.com/%s/permissions' % facebook_id
+  h = httplib2.Http()
+  result = h.request(url, 'DELETE')[1]
+  del login_session['username']
+  del login_session['email']
+  del login_session['picture']
+  del login_session['user_id']
+  del login_session['facebook_id']
+  return "you have been logged out"
+
+
+
+@app.route('/disconnect')
+def disconnect():
+  if 'provider' in login_session:
+    if login_session['provider'] == 'google':
+      gdisconnect()
+      del login_session['gplus_id']
+      del login_session['credentials']
+    if login_session['provider'] == 'facebook':
+      fbdisconnect()
+      del login_session['facebook_id']
+    del login_session['username']
+    del login_session['email']
+    del login_session['picture']
+    del login_session['user_id']
+    del login_session['provider']
+    flash("you have successfully been logged out")
+    return redirect(url_for('showRestaurants'))
+  else:
+    flash("You were not logged in to begin with")
+    redirect(url_for('showRestaurants'))
+
 
 
 #JSON APIs to view Restaurant Information
